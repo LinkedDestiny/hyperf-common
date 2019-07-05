c9c750f1c5e16b3b5b72678092da6da72f764db5
lidanyang <lidanyang@bilibili.com>
2019-07-05 16:03:35 +0800

    init

---
+++ src/Listener/FetchModelListener.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+
+namespace Lib\Listener;
+
+
+use Hyperf\Database\Events\StatementPrepared;
+use Hyperf\Event\Annotation\Listener;
+use Hyperf\Event\Contract\ListenerInterface;
+
+/**
+ * Class FetchModelListener
+ * @package Lib\Listener
+ * @Listener
+ */
+class FetchModelListener implements ListenerInterface
+{
+
+    /**
+     * @return string[] returns the events that you want to listen
+     */
+    public function listen(): array
+    {
+        return [
+            StatementPrepared::class
+        ];
+    }
+
+    /**
+     * Handle the Event when the event is triggered, all listeners will
+     * complete before the event is returned to the EventDispatcher.
+     * @param object $event
+     */
+    public function process(object $event)
+    {
+        if ($event instanceof StatementPrepared) {
+            $event->statement->setFetchMode(\PDO::FETCH_ASSOC);
+        }
+    }
+}
