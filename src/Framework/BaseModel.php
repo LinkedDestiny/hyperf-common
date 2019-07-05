336d370f4ce9fa112ef882cf9294a456a563b8a1
lidanyang <lidanyang@bilibili.com>
2019-07-05 15:56:11 +0800

    init

--- src/Framework/BaseModel.php
+++ src/Framework/BaseModel.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 
 namespace Lib\Framework;
 
+use Hyperf\Database\Model\Builder;
 use Hyperf\DbConnection\Model\Model;
 
 class BaseModel extends Model
@@ -14,4 +15,31 @@ class BaseModel extends Model
     const UPDATED_AT = 'update_at';
 
     protected $dateFormat = 'U';
+
+    /**
+     * @param $key
+     * @param $value
+     * @return Builder|static|object|null
+     */
+    public static function findOne($key, $value)
+    {
+        return self::query()
+            ->where($key, $value)
+            ->first();
+    }
+
+    public function insert(array $data)
+    {
+        $this->fill($data);
+
+        return $this->save();
+    }
+
+    public function disable()
+    {
+        $this->fill([
+            'enable' => 1
+        ]);
+        return $this->save();
+    }
 }
